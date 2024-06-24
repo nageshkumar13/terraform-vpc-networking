42316f716346fb7beb3f8a478df659235ce8ec57
nageshkumar13 <nageshkumar1925@gmail.com>
2024-06-24 14:43:14 +0530

    updated

---
+++ infra/vpc/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source                             = "../../modules/vpc"
+
+  vpc_cidr_block                   =          "10.0.0.0/16"
+  instance_tenancy                 =          "default"
+  enable_dns_hostnames             =          true 
+  assign_generated_ipv6_cidr_block =          true
+  vpc_tag_name                     =          "Matrix"
+  public_subnet_cidr_block         =          ["10.0.1.0/24", "10.0.3.0/24"]
+  private_subnet_cidr_block        =          ["10.0.2.0/24", "10.0.4.0/24"]
+  map_public_ip_on_launch          =          false
+
+}
+
+
